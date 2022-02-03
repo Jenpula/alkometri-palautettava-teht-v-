f67b50787c9ecbe2c7a8e99a6a8b3a5f4f25f9c9
Annala <n1anje00@students.oamk.fi>
2022-02-03 09:16:28 +0200

    Removed excessive pictures and added comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -8,13 +8,12 @@ function App() {
   const [gender, setGender] = useState('male');
   const [result, setResult] = useState(0);
 
+// Calculating alcohol level based on gender
 const calculate = () => {
- 
   const litres = bottles * 0.33;
   const burning = weight / 10;
   const grams = litres * 8 * 4.5
   const left = grams - (burning * time)
-
   let alcohol = 0;
 
   if(gender === 'male') {
@@ -22,7 +21,7 @@ const calculate = () => {
   } else {
     alcohol = left / (weight * 0.6);
   }
-   if(alcohol < 0 ) {
+  if(alcohol < 0 ) {
     setResult(0);
   } else {
     setResult(alcohol);
@@ -41,50 +40,50 @@ const calculate = () => {
       </div>
       <div>
       <label>Bottles</label>
-        <select
+      <select
         type='number'
         value={bottles}
         onChange={e => setBottles(e.target.value)}>
           <option value='0'>0</option>
-         <option value='1'>1</option>
-         <option value='2'>2</option>
-         <option value='3'>3</option>
-         <option value='4'>4</option>
-         <option value='5'>5</option>
-         <option value='6'>6</option>
-         <option value='7'>7</option>
-         <option value='8'>8</option>
-         <option value='9'>9</option>
-         <option value='10'>10</option>    
-         <option value='11'>11</option>
-         <option value='12'>12</option>
-         <option value='13'>13</option>
-         <option value='14'>14</option>
-         <option value='15'>15</option>
-         <option value='16'>16</option>
-         <option value='17'>17</option>
-         <option value='18'>18</option>
-         <option value='19'>19</option>
-         <option value='20'>20</option>
-         </select>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+          <option value='6'>6</option>
+          <option value='7'>7</option>
+          <option value='8'>8</option>
+          <option value='9'>9</option>
+          <option value='10'>10</option>    
+          <option value='11'>11</option>
+          <option value='12'>12</option>
+          <option value='13'>13</option>
+          <option value='14'>14</option>
+          <option value='15'>15</option>
+          <option value='16'>16</option>
+          <option value='17'>17</option>
+          <option value='18'>18</option>
+          <option value='19'>19</option>
+          <option value='20'>20</option>
+        </select>
         </div>
         <div>
       <label>Time</label>
         <select
-         type='number'
-         value={time}
-         onChange={e => setTime(e.target.value)}>
-         <option value='0'>0</option>
-         <option value='1'>1</option>
-         <option value='2'>2</option>
-         <option value='3'>3</option>
-         <option value='4'>4</option>
-         <option value='5'>5</option>
-         <option value='6'>6</option>
-         <option value='7'>7</option>
-         <option value='8'>8</option>
-         <option value='9'>9</option>
-         <option value='10'>10</option>
+          type='number'
+          value={time}
+          onChange={e => setTime(e.target.value)}>
+            <option value='0'>0</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
          </select>
         </div>
         <div>
@@ -96,10 +95,9 @@ const calculate = () => {
         </div>
         <output>{result.toFixed(1)}</output>
         <div>
-        <button type='button' class='btn btn-dark' onClick={calculate}>calculate</button>
+        <button type='button' class='btn btn-dark' onClick={calculate}>Calculate</button>
         </div>
         </div>
   );
 }
-
 export default App;
